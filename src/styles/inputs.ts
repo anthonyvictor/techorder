b87ff606e8e845920dfc0d1854d2cc5e6503bcc4
Anthony Victor <thonnyvrc@gmail.com>
2022-05-13 16:09:50 -0300

    some codelines

--- src/styles/inputs.ts
+++ src/styles/inputs.ts
@@ -8,14 +8,12 @@ export const InputWithLabelContainer = styled.section.attrs((props: {label: stri
           top: 0.4rem;
           left: 0;
           padding-left: 0.4rem;
-          color: ${(props) => props.theme.fontSub};
           transition: all 0.1s ease-in-out;
           pointer-events: none;
           
           font-size: 0.7rem;
           padding: 0;
           transform: translateY(-0.7rem);
-          color: ${(props) => props.theme.backSub};
         }
 
         input {
@@ -25,6 +23,14 @@ export const InputWithLabelContainer = styled.section.attrs((props: {label: stri
           border-bottom: 1px solid black;
           padding: 5px;
         }
+
+        input:-webkit-autofill,
+input:-webkit-autofill:focus {
+    transition: background-color 600000s 0s, color 600000s 0s;
+}
+input[data-autocompleted] {
+    background-color: transparent !important;
+}
 
         small{
           
